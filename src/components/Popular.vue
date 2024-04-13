f3705d5b12c4b4068685a8aea6cb6202c46c6073
Levani migriauli <levani.migriauli@lb.ge>
2024-04-13 17:31:47 +0400

    feat: add popular component

---
+++ src/components/Popular.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ComponentWrapper from '../ui/shared/ComponentWrapper.vue'
+import popularImg from '../assets/images/popular.png'
+import { ref } from 'vue'
+
+const sectionPopularContent = ref([
+  { id: '1', image: popularImg, textContent: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა' },
+  { id: '2', image: popularImg, textContent: 'ასაწონად დღეიდან განკუთვნილი პოლიეთილენის პარკები აიკრძალა' },
+  { id: '3', image: popularImg, textContent: 'დღეიდან ასაწონად განკუთვნილი პოლიეთილენის პარკები აიკრძალა' },
+  { id: '4', image: popularImg, textContent: 'ასაწონად დღეიდან განკუთვნილი პოლიეთილენის პარკები აიკრძალა' }
+])
+
+</script>
+
+<template>
+  <ComponentWrapper>
+    <section class="pt-[25px] pl-4 pr-3">
+      <h3 class="text-headline2Mobile mb-[27px]">პოპულარული</h3>
+      <div class="max-h-[330px] overflow-auto">
+        <div v-for="(item, index) in sectionPopularContent" :key="item.id"
+             class="flex items-center gap-2 pb-4"
+             :class="{ 'pt-4': index !== 0, 'border-b-2 border-dashed' : index !==sectionPopularContent.length - 1 , 'pt-0': index === 0 }">
+          <img :src="item.image" :alt="`image ${index + 1}`"/>
+          <p class="text-headline3">{{ item.textContent }}</p>
+        </div>
+      </div>
+    </section>
+  </ComponentWrapper>
+</template>
